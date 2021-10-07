4d12bda83ec03aa4091f0b35b676598ac8c98486
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-10-07 13:30:45 +0200

    Second commit!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //skriver jeg noget som du får?
+        //skriver jeg noget som du får? Ja
     }
 }
