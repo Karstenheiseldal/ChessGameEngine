034d1693084e51769aa6c3406bb1a0380cc3e08a
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-10-11 18:39:29 +0200

    Changed variable names for the coordinates to x-y

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        //skriver jeg noget som du får? Ja
+	Spot tiles = new Spot();
+	tiles.makeSpots();
     }
 }
