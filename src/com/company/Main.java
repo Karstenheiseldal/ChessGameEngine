9fe601f97191c201ee3e6bd12cf0c3edba898a22
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-10-19 22:57:14 +0200

    Boardgrid class is working, check main to play with the arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,9 @@ public class Main {
 
 		Boardgrid myBoard = new Boardgrid();
 		Spot spotfromindex = myBoard.spotArray[6][4];
-		System.out.println(spotfromindex.getPiece());
+		System.out.println(spotfromindex.piece.getClass().getSimpleName());
 		System.out.println("x: "+spotfromindex.getX());
 		System.out.println("y: " +spotfromindex.getY());
 		System.out.println("White: " +spotfromindex.piece.getWhite());
-
 	}
 }
