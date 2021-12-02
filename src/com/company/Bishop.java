884ee098300e888f26bb4762f0592115fc3a0511
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-12-02 12:22:16 +0100

    Moving bishop

--- src/com/company/Bishop.java
+++ src/com/company/Bishop.java
@@ -13,59 +13,64 @@ public class Bishop extends Piece {
 
     @Override
     public boolean legalMoveCheck(Spot start, Spot end, Boardgrid b) {
-        boolean bishopMove = true;
+
         if (abs(start.getX() - end.getX()) == abs(start.getY() - end.getY())) { //if the absolute difference between the x-move and y-move
 
             if (start.getY() < end.getY()) { //When the y-axis goes down (higher number)
                 if (start.getX() < end.getX()) { //and the x-axis goes right (higher number)
                     //looping through the spots between start and end, and checking if it's occupied
                     for (int y = start.getY() + 1, x = start.getX() + 1; y <= end.getY() && x <= end.getX(); y++, x++) {
-                        if (b.spotArray[y][x].isOccupied && !start.piece.getWhite() != end.piece.getWhite()) { //double negation
-                            System.out.println("1:  it's true since y is " + y + " and x is " + x);
-                            bishopMove = false;
+                        if (b.spotArray[y][x].isOccupied && start.piece.getWhite() == end.piece.getWhite()) { //double negation
+                            return false;
+                        }
+                        if (b.spotArray[y][x].isOccupied && start.piece.getWhite() != b.spotArray[x][y].piece.getWhite()){
+                            return true;
                         }
                     }
-
                 }
+
                 if (start.getX() > end.getX()) { //and when the x-axis goes left (lower number)
                     //looping through the spots between start and end, and checking if it's occupied
-                    for (int y = start.getY() + 1, x = start.getX() - 1; y <= end.getY() && x >= end.getX(); y++, x--) {
-                        if (b.spotArray[y][x].isOccupied && !start.piece.getWhite() != end.piece.getWhite()) { //double negation
+                    for (int y = start.getY() - 1, x = start.getX() - 1; y <= end.getY() && x >= end.getX(); y++, x--) {
+                        if (b.spotArray[y][x].isOccupied && b.spotArray[y][x].getX() !=end.getX() && b.spotArray[y][x].getY() != end.getY()) {
                             System.out.println("2  it's true since y is " + y + " and x is " + x);
-                            bishopMove = false;
+                            return false;
+                        }
+                        if(b.spotArray[y][x].isOccupied && start.piece.getWhite() != end.piece.getWhite()){
+                            return true;
                         }
                     }
-
                 }
             }
             if (start.getY() > end.getY()) { //when the y axis goes up (lower number)
                 if (start.getX() < end.getX()) { //and the x-axis goes right (higher number)
                     //looping through the spots between start and end, and checking if it's occupied
                     for (int y = start.getY() - 1, x = start.getX() + 1; y >= end.getY() && x <= end.getX(); y--, x++) {
-                        if (b.spotArray[y][x].isOccupied && !start.piece.getWhite() != end.piece.getWhite()) { //double negation
-                            System.out.println("3  it's true since y is " + y + " and x is " + x);
-                            bishopMove = false;
-
+                        if (b.spotArray[y][x].isOccupied && start.piece.getWhite() == end.piece.getWhite()) { //double negation
+                            //System.out.println("3  it's true since y is " + y + " and x is " + x);
+                            return false;
+                        }
+                        if(b.spotArray[y][x].isOccupied && start.piece.getWhite() != end.piece.getWhite()){
+                            return true;
                         }
                     }
-
                 }
                 if (start.getX() > end.getX()) { //and when the x-axis goes left (lower number)
                     //looping through the spots between start and end, and checking if it's occupied
                     for (int y = start.getY() - 1, x = start.getX() - 1; y >= end.getY() && x >= end.getX(); y--, x--) {
-                        if (b.spotArray[y][x].isOccupied && !start.piece.getWhite() != end.piece.getWhite()) { //double negation
+                        if (b.spotArray[y][x].isOccupied && start.piece.getWhite() == end.piece.getWhite()) { //double negation
                             System.out.println("4 it's true since y is " + y + " and x is " + x);
-                            bishopMove = false;
+                            return false;
+                        }
+                        if (b.spotArray[y][x].isOccupied && start.piece.getWhite() != end.piece.getWhite()){
+                            return true;
                         }
                     }
 
                 }
             }
-
-            if (bishopMove) {
-                return true;
+            return true;
             }
-        }
         return false;
+        }
     }//end of legelMoveCheck
-}
