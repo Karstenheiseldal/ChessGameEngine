4babc5b9951b378653dcab320b62363928be352f
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-12-06 20:03:56 +0100

    Knight is coming together

--- src/com/company/Knight.java
+++ src/com/company/Knight.java
@@ -1,4 +1,5 @@
 package com.company;
+import static java.lang.Math.abs;
 
 public class Knight extends Piece{
 
@@ -8,6 +9,18 @@ public class Knight extends Piece{
 
     @Override
     public boolean legalMoveCheck(Spot start, Spot end, Boardgrid boardgrid) {
+        if(end.isOccupied && end.piece.getWhite() == start.piece.getWhite()){
+            return false;
+        }
+
+        if (abs(start.getY() - end.getY()) == 1 && abs(start.getX() - end.getX()) == 2){
+            return true;
+        }
+
+        if (!end.isOccupied && abs(start.getY() - end.getY()) == 2 && abs(start.getX() - end.getX()) == 1){
+            return true;
+        }
+
         return false;
     }
 }
