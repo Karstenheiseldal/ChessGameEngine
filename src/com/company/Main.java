f603e6579fa5cff119128f6ba5f204648bd4021a
Karstenheiseldal <karstenheiseldal@outlook.com>
2021-12-11 20:16:27 +0100

    The game is now turn based. It also exits when a king is slaughtered

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,28 +2,56 @@ package com.company;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		boolean gamestart = true;
+		boolean gameStart = true;
 		Boardgrid myboard = new Boardgrid();
 		Player player1 = new Player1(true);
 		Player player2 = new Player2(false);
 		Display display = new Display();
+		Integer gamePhase = 1;
 
 		display.display(myboard);
 		System.out.println("choose new move");
+		while (gameStart) {
+			if (gamePhase == 1)
+				try {
+					System.out.println("Player 1 Turn");
+					Display.moveList.clear();
+					while (Display.moveList.size() < 3) Thread.sleep(1000);
+					Move move = new Move(myboard, player1, myboard.spotArray[Display.moveList.get(0)][Display.moveList.get(1)], myboard.spotArray[Display.moveList.get(2)][Display.moveList.get(3)]);
+					if (move.movingPiece.getWhite() != player1.white) {
+						System.out.println("Player one has White pieces");
+						Display.moveList.clear();
+						gamePhase = 1;
+					} else {
+						move.makeMove();
+						Display.moveList.clear();
+						display.updateFrame();
+						gamePhase = 2;
+					}
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+			if (gamePhase == 2) {
+				try { //Player 2 turn
+					System.out.println("Player 2 turn");
+					Display.moveList.clear();
+					while (Display.moveList.size() < 3) Thread.sleep(1000);
+					Move move = new Move(myboard, player2, myboard.spotArray[Display.moveList.get(0)][Display.moveList.get(1)], myboard.spotArray[Display.moveList.get(2)][Display.moveList.get(3)]);
+					if (move.movingPiece.getWhite() != player2.white) {
+						System.out.println("Player two has Black pieces");
+						Display.moveList.clear();
+						gamePhase = 2;
+					} else {
+						move.makeMove();
+						System.out.println(move.movingPieceName + " " + Display.moveList.get(0) + "," + Display.moveList.get(1) + " to " + Display.moveList.get(2) + "," + Display.moveList.get(3));
+						display.updateFrame();
+						gamePhase = 1;
+					}
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
 
-		while (gamestart) {
-
-			try {
-				Display.moveList.clear();
-				while(Display.moveList.size()<3) Thread.sleep(1000);
-				Move move = new Move(myboard,player1,myboard.spotArray[Display.moveList.get(0)][Display.moveList.get(1)], myboard.spotArray[Display.moveList.get(2)][Display.moveList.get(3)]);
-				move.makeMove();
-				System.out.println(move.movingPieceName + " " + Display.moveList.get(0) + "," + Display.moveList.get(1) + " to " + Display.moveList.get(2) + "," + Display.moveList.get(3));
-				display.updateFrame();
-
-			} catch (Exception e) {
-				System.out.println(e.getMessage());
 			}
 		}
 	}
-}
+	}
